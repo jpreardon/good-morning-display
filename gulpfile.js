e79eb61474cc9385bd29846047ab91ab5af82e6d
John P. Reardon <jp@jpreardon.com>
2015-12-22 16:04:00 -0500

    Watch and browsersync, remove unused requires

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 var gulp = require('gulp')
 var del = require('del')
-var babelify = require('babelify');
 
-
-// The following requires were taken from the Browserify + Transforms
-// recipe: https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-transforms.md
+// The following requires were taken from the Wes Bos's files
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 var buffer = require('vinyl-buffer')
 var gutil = require('gulp-util')
 var uglify = require('gulp-uglify')
-var sourcemaps = require('gulp-sourcemaps')
-var reactify = require('reactify')
+var babelify = require('babelify');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var historyApiFallback = require('connect-history-api-fallback')
+var watchify = require('watchify')
+var notify = require('gulp-notify')
 
 var paths = {
   html : 'index.html',
-  css : ['css/app.css', 'css/normalize.css', 'css/foundation.min.css'],
+  styles : 'css/**/*',
   scripts : 'scripts/main.js'
 }
 
@@ -24,16 +25,54 @@ gulp.task('clean', function() {
   return del(['build/*'])
 })
 
-// Copy index.html
-gulp.task('copy-html', ['clean'], function() {
-  gulp.src(paths.html)
-    .pipe(gulp.dest('build/'))
+// Copy Styles
+gulp.task('styles', function() {
+  gulp.src(paths.styles)
+    .pipe( gulp.dest('build/css') )
 })
 
-// Copy CSS
-gulp.task('copy-css', ['clean'], function() {
-  gulp.src(paths.css)
-    .pipe( gulp.dest('build/css') )
+// Stolen from Wes Bos's file
+function buildScript(file, watch) {
+  var props = {
+    entries: ['./scripts/' + file],
+    debug : true,
+    transform:  babelify.configure({presets: ["es2015", "react"]})
+  };
+
+  // watchify() if watch requested, otherwise run browserify() once
+  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream
+      .on('error', handleErrors)
+      .pipe(source(file))
+      .pipe(gulp.dest('./build/scripts/'))
+      // If you also want to uglify it
+      // .pipe(buffer())
+      // .pipe(uglify())
+      // .pipe(rename('app.min.js'))
+      // .pipe(gulp.dest('./build'))
+      .pipe(reload({stream:true}))
+  }
+
+  // listen for an update and run rebundle
+  bundler.on('update', function() {
+    rebundle()
+    gutil.log('Rebundle...')
+  })
+
+  // run it once the first time buildScript is called
+  return rebundle();
+}
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        // we need to disable clicks and forms for when we test multiple rooms
+        server : {},
+        middleware : [ historyApiFallback() ],
+        ghostMode : false
+    })
 })
 
 function handleErrors() {
@@ -45,32 +84,12 @@ function handleErrors() {
   this.emit('end'); // Keep gulp from hanging on this task
 }
 
-// Copy Scripts (will run through babel at some point with .pipe(babelfy()) before the dest)
-// From browserify + transforms recipe
-gulp.task('copy-scripts', ['clean'], function() {
-
-  var b = browserify({
-    entries : 'scripts/main.js',
-    debug : true,
-    transform : babelify.configure({presets: ["es2015", "react"]})
-  })
-
-  return b.bundle()
-    .pipe(source('scripts/main.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-      //.pipe(uglify())
-      .on('error', handleErrors)
-    //.pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./build/'))
-})
+gulp.task('scripts', function() {
+  return buildScript('main.js', false); // this will run once because we set watch to false
+});
 
 // The default task
-gulp.task('default', ['copy-html', 'copy-css', 'copy-scripts'], function() {
-
+gulp.task('default', ['clean', 'styles', 'scripts', 'browser-sync'], function() {
+  gulp.watch('css/**/*', ['styles'])
+  return buildScript('main.js', true)
 })
-
-
-// 1A) In the process of doing that, I want to send them through babel to convert JSX and ES6 to ES5
-// spin up a local server and browser sync
-// Watch for changed files and run through the build process each time (sync browser)
--- scripts/components/App.js
+++ scripts/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <p>Hello world, suckas!</p>
+      <p>Hello world! (finally)</p>
     )
   }
 }
