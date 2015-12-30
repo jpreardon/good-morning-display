8fd9ebad4cdeca900e164167ad2afaa4c5119411
John P. Reardon <jp@jpreardon.com>
2015-12-30 17:52:26 -0500

    Wire up TransitStatus component to data. Refactor polling so weather and transit are called at different intervals.

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -7,6 +7,7 @@ import TransitStatus from './TransitStatus'
 
 // TODO: This needs to go elsewhere, I might put it back in localStorage at some point, for now, I leave it
 const WEATHER_API = 'http://api.wunderground.com/api/1197e676c4b523c6/conditions/forecast/hourly/q/11238.json'
+const TRANSIT_API = 'http://jpreardon.com/gmd/cgi-bin/service_status.py'
 
 // Update frequency constants
 const WEATHER_UPDATE_FREQ = 1800000 // In milliseconds (every 30 minutes)
@@ -15,23 +16,59 @@ const TRANSIT_UPDATE_FREQ = 120000 // In milliseconds (every 2 minutes)
 class App extends React.Component {
   constructor() {
     super()
-    this.state = { current : {}, icon_url : '', forecasts : [], updateTime : '' }
+    this.state = {
+      current : {},
+      icon_url : '',
+      forecasts : [],
+      weatherUpdateTime : '',
+      transit : [],
+      transitUpdateTime : ''
+     }
   }
 
-  loadDataFromServer() {
+  loadWeatherData() {
     u.fetchData(WEATHER_API, (result) => {
       this.setState({
         current: result.current_observation,
         icon_url: `http://icons.wxug.com/i/c/i/${result.current_observation.icon}`,
         forecasts: result.hourly_forecast,
-        updateTime: new Date().toLocaleTimeString()
+        weatherUpdateTime: new Date().toLocaleTimeString()
       })
     })
   }
 
+  loadTransitData() {
+    u.fetchData(TRANSIT_API, (result) => {
+      this.setState({
+        transit: result,
+        transitUpdateTime: new Date().toLocaleTimeString()
+      })
+    })
+  }
+
+  refreshData() {
+    var date = new Date()
+  	// Update the weather information, if needed
+  	if ((!localStorage.getItem("lastWeatherUpdate")) || (Number(localStorage.lastWeatherUpdate) + WEATHER_UPDATE_FREQ < date.getTime())) {
+  		// Update weather!
+  		this.loadWeatherData()
+  		// Set the last update time
+  		localStorage.lastWeatherUpdate = date.getTime()
+  	}
+
+  	// Update the transit information, if needed
+  	if ((!localStorage.getItem("lastTransitUpdate")) || (Number(localStorage.lastTransitUpdate) + TRANSIT_UPDATE_FREQ < date.getTime())) {
+  		// Update transit!
+  		this.loadTransitData()
+  		// Set the last update time
+  		localStorage.lastTransitUpdate = date.getTime()
+  	}
+  }
+
   componentDidMount() {
-    this.loadDataFromServer()
-    setInterval(this.loadDataFromServer.bind(this), WEATHER_UPDATE_FREQ)
+    this.loadWeatherData()
+    this.loadTransitData()
+    setInterval(this.refreshData.bind(this), 1000)
   }
 
   render() {
@@ -40,12 +77,12 @@ class App extends React.Component {
         <div className="weather-container">
           <CurrentConditions current={this.state.current} icon_url={this.state.icon_url} />
           <Forecast forecasts={this.state.forecasts} />
-          <UpdateTime time={this.state.updateTime} />
         </div>
+        <UpdateTime time={this.state.weatherUpdateTime} />
         <div className="transit-container">
-          <TransitStatus />
-          <UpdateTime time="never!" />
+          <TransitStatus transitStatus={this.state.transit}/>
         </div>
+        <UpdateTime time={this.state.transitUpdateTime} />
       </div>
     )
   }
--- scripts/components/TransitStatus.js
+++ scripts/components/TransitStatus.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
 class TransitStatus extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+  }
+
+  prettyStatus(uglyStatus) {
+    switch (uglyStatus) {
+      case 'SERVICE CHANGE':
+        return 'Service Change'
+      case 'GOOD SERVICE':
+        return 'Good Service'
+      case 'DELAYS':
+        return 'Delays'
+      default:
+        return uglyStatus
+    }
   }
 
   render() {
+    var statusObject = new Object
+    this.props.transitStatus.forEach( data => {
+      statusObject[data.name[0]] = this.prettyStatus(data.status[0])
+    })
     return (
       <div>
         <table>
@@ -16,7 +33,7 @@ class TransitStatus extends React.Component {
                 <span className="mta-bullet mta-2">2</span>
                 <span className="mta-bullet mta-3">3</span>
               </td>
-              <td className="service-status">Good Service</td>
+              <td className="service-status">{statusObject['123']}</td>
             </tr>
             <tr>
               <td className="train-line">
@@ -24,13 +41,13 @@ class TransitStatus extends React.Component {
                 <span className="mta-bullet mta-5">5</span>
                 <span className="mta-bullet mta-6">6</span>
               </td>
-              <td className="service-status">Good Service</td>
+              <td className="service-status">{statusObject['456']}</td>
             </tr>
             <tr>
               <td className="train-line">
                 <span className="mta-bullet mta-7">7</span>
               </td>
-              <td className="service-status">Good Service</td>
+              <td className="service-status">{statusObject['7']}</td>
             </tr>
             <tr>
               <td className="train-line">
@@ -38,7 +55,7 @@ class TransitStatus extends React.Component {
                 <span className="mta-bullet mta-c">C</span>
                 <span className="mta-bullet mta-e">E</span>
               </td>
-              <td className="service-status">Good Service</td>
+              <td className="service-status">{statusObject['ACE']}</td>
             </tr>
             <tr>
               <td className="train-line">
@@ -47,34 +64,34 @@ class TransitStatus extends React.Component {
                 <span className="mta-bullet mta-f">F</span>
                 <span className="mta-bullet mta-m">M</span>
               </td>
-              <td className="service-status">Delays</td>
+              <td className="service-status">{statusObject['BDFM']}</td>
             </tr>
             <tr>
               <td className="train-line">
                 <span className="mta-bullet mta-j">J</span>
                 <span className="mta-bullet mta-z">Z</span>
               </td>
-              <td className="service-status">Delays</td>
+              <td className="service-status">{statusObject['JZ']}</td>
             </tr>
             <tr>
               <td className="train-line">
                 <span className="mta-bullet mta-l">L</span>
               </td>
-              <td className="service-status">Delays</td>
+              <td className="service-status">{statusObject['L']}</td>
             </tr>
             <tr>
               <td className="train-line">
                 <span className="mta-bullet mta-n">N</span>
-                <span className="mta-bullet mta-r">R</span>
                 <span className="mta-bullet mta-q">Q</span>
+                <span className="mta-bullet mta-r">R</span>
               </td>
-              <td className="service-status">Delays</td>
+              <td className="service-status">{statusObject['NQR']}</td>
             </tr>
             <tr>
               <td className="train-line">
                 <span className="mta-bullet mta-s">S</span>
               </td>
-              <td className="service-status">Delays</td>
+              <td className="service-status">{statusObject['S']}</td>
             </tr>
           </tbody>
         </table>
